0cab5b3135232fd086d0e852a849b733a7058d0d
Ajazsakib <saquibajaz999@gmail.com>
2023-08-09 00:03:08 +0530

    implement authentication using passport js

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ const port = 8000;
 
 const db = require("./config/mongoose");
 
+const session = require("express-session");
+
+const passport = require("passport");
+
+const passportLocal = require("./config/passport-local-strategy");
+
+const MongoStore = require("connect-mongo")(session);
+
+const sassMiddleware = require("sass-middleware");
+
+app.use(
+  sassMiddleware({
+    src: "./assets/scss", // Source directory of your Sass files
+    dest: "./assets/css", // Destination directory for compiled CSS
+    debug: true, // Enable debugging
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
@@ -20,12 +40,43 @@ app.set("layout extractScripts", true);
 
 app.use(express.static("./assets"));
 
-// use express router
-app.use("/", require("./routes/index"));
-
 // setup the view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+// mongo store is used to store the session cookie in the db
+
+// use session
+
+app.use(
+  session({
+    name: "Social Media",
+    // Todo change  the secret before deployment
+    secret: "mylittlesecret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "Connect Mongo DB Setup ok");
+      }
+    ),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use("/", require("./routes/"));
 
 app.listen(port, function (err) {
   if (err) {
